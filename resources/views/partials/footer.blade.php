94b476509990b1994bb44ca54618964240f55f36
alessandrob17 <a.baldipardi@gmail.com>
2023-04-11 18:30:35 +0200

    FEAT blade

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,89 @@
+<footer>
+    <div>
+        <ul>
+            <li>
+                <h4>
+                    DC COMICS
+                </h4>
+            </li>
+            <li>
+                <a href="">Characters</a>
+            </li>
+            <li>
+                <a href="">Comics</a>
+            </li>
+            <li>
+                <a href="">Movies</a>
+            </li>
+            <li>
+                <a href="">Tv</a>
+            </li>
+            <li>
+                <a href="">Games</a>
+            </li>
+            <li>
+                <a href="">Videos</a>
+            </li>
+            <li>
+                <a href="">News</a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <h4>SHOP</h4>
+            </li>
+            <li>
+                <a href="">Shop DC</a>
+
+            </li>
+            <li>
+                <a href="">Shop collectibles</a>
+
+            </li>
+
+        </ul>
+
+        <ul>
+            <li>
+                <h4>
+                    DC
+                </h4>
+            </li>
+            <li>
+                <a href="">Terms of use</a>
+            </li>
+            <li>
+                <a href="">Privacy</a>
+            </li>
+            <li>
+                <a href="">Choices</a>
+            </li>
+            <li>
+                <a href="">Advertisment</a>
+            </li>
+            <li>
+                <a href="">Jobs</a>
+            </li>
+            <li>
+                <a href="">Subscription</a>
+            </li>
+            <li>
+                <a href="">Workshops</a>
+            </li>
+            <li>
+                <a href="">CPSC</a>
+            </li>
+            <li>
+                <a href="">ratings</a>
+            </li>
+            <li>
+                <a href="">Shop help</a>
+            </li>
+            <li>
+                <a href="">Contact us</a>
+            </li>
+        </ul>
+
+    </div>
+</footer>
